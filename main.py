c576cf03173f98142ee8bc5c47967f3f7af43c76
Xaliq <xaliq2299@mail.ru>
2019-12-18 14:48:06 +0400

    some changes

--- main.py
+++ main.py
@@ -15,16 +15,31 @@ def read_file(filename, separator, headerExist = False):
 
 
 def main():
+	
 	data_from_file = np.array(read_file("heart_disease_dataset.csv", ';', True))
+	nbEpochs=int(input("Enter the number of epochs: "))
 	neuron = NeuralNet(data_from_file, 2, 8, 5)
-	print("Enter \"r\" for repeating tests(15 epochs)")
-	option = input()
-	while option == 'r':
-		neuron.train(15)
-		print("\nEnter \"r\" for repeating tests(15 epochs)")
-		option = input()
+	print("Enter \n\"r\" for running tests (",nbEpochs, "epochs ) or \n\"b\" for the best accuracy result of last testing")
 
-	print('Best result achieved during tests:' + str(neuron.bestResult))
+	option = input()
+	while option == 'r' or option == 'b':
+		if option == 'r':
+			neuron.train(nbEpochs)
+		elif option == 'b':
+			print('Best result achieved during last tests: ' + str(neuron.bestResult))
+			#print(neuron.W1)
+		print("Enter \n\"r\" for running tests (",nbEpochs, "epochs ) or \n\"b\" for the best accuracy result of last testing")
+		option = input()
+	
+	#print(neuron.W1)
+	print("Writing best weights output file")
+	filename_weights='best_weights.npz'
+	np.savez(filename_weights, name1=neuron.W1, name2=neuron.W2, name3=neuron.b1, name4=neuron.b2)
+	data = np.load(filename_weights) # reading for future use
+	print(data['name1'])
+	print(data['name2'])
+	print(data['name3'])
+	print(data['name4'])
 
 
 main()
